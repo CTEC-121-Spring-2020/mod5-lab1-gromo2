9af68748d194691849990b72b5de301f8bb2e1fd
Garrett <g.romo2@students.clark.edu>
2020-05-18 11:50:51 -0700

    completed lab

--- Template.py
+++ Template.py
@@ -1,17 +1,75 @@
 """
 CTEC 121
-<your name>
+<Garrett>
 <assignment/lab name>
 <assignment/lab description
 """
 
 """ IPO template
-Input(s): list/description
-Process: description of what function does
-Output: return value and description
+Input(s): none
+Process: prints/outputs chorus of happy birthday song
+Output: prints to terminal
 """
 
-def main():
-    # code here
+def main1():
+    print()
+    print("Happy birthday to you!")
+    print("Happy birthday to you!")
+    print("Happy birthday dear Fred...")
+    print("Happy birthday to you!")
+    print()
 
-main()    
+# remove obvious duplication/redundancy
+""" IPO happy()
+Input(s): none
+Process: prints/outputs chorus of happy birthday song
+Output: prints to terminal screen
+"""
+def happy():
+    print("Happy birthday to you!")
+def main2():
+    print()
+    happy()
+    happy()
+    print("Happy birthday, dear Fred...")
+    happy()
+    print()
+
+
+# generalize for any person
+""" IPO happyBDay()
+Input(s): a name
+Process: prints/outputs verse line of happy birthday song
+Output: prints to terminal screen
+"""
+def happyBDay(name):
+    print("Happy birthday, day ", name, "...", sep="")
+
+def main3():
+    print()
+    happy()
+    happy()
+    happyBDay("Fred")
+    happy()
+    print()
+
+""" IPO happyBDay()
+Input(s): a name
+Process: prints/outputs happy birthday song
+Output: prints to terminal screen
+"""
+# combine song into a function
+def singHappyBDay(name):
+    happy()
+    happy()
+    happyBDay(name)
+    happy()
+    print()
+
+def main4():
+    print()
+    singHappyBDay("Fred")
+    singHappyBDay("Lucy")
+    singHappyBDay("Bill")
+    print()
+main4()   
